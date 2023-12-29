2e1918e1a1529136775954b97e89dfe014f69aea
zhf521 <3407085928@qq.com>
2023-12-29 16:41:41 +0800

    完成基本页面布局

---
+++ src/views/AddMusicView.vue
@@ -0,0 +1,24 @@
+<template>
+  <!-- WebDav未设置 -->
+  <div v-if="false" class="not-set">
+    <el-empty description="WebDav服务器未配置或配置有误">
+      <el-button type="primary" @click="goToSet">点我去设置</el-button>
+    </el-empty>
+  </div>
+  <!-- 文件管理 -->
+  <div v-else class="file-manager">
+    文件管理器
+  </div>
+</template>
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+
+// 引入路由器
+const router = useRouter();
+
+// 跳转到设置
+const goToSet = () => {
+  router.push('/setting')
+}
+</script>
+<style scoped></style>
