86ee6454474b50aa64160c8e807ecf250299412f
zhf521 <3407085928@qq.com>
2023-12-29 16:42:17 +0800

    完善页面基本结构

--- src/views/MusicLibraryView.vue
+++ src/views/MusicLibraryView.vue
@@ -1,6 +1,27 @@
 <template>
-  音乐库
+  <div v-show="route.meta.showFather">
+    <el-empty v-if="true" description="空空如也~">
+      <el-button type="primary" @click="addMusic">点我去添加</el-button>
+    </el-empty>
+    <div v-else class="music-list">
+      <MusicList />
+    </div>
+  </div>
+  <router-view></router-view>
 </template>
 <script setup lang="ts">
+import { useRoute, useRouter } from 'vue-router';
+
+// 引入路由和路由器
+const route = useRoute();
+const router = useRouter();
+// 添加音乐到音乐库
+const addMusic = () => {
+  router.push('/music-library/add-music')
+}
 </script>
-<style scoped></style>
+<style scoped>
+.music-list {
+  height: 100%;
+}
+</style>
