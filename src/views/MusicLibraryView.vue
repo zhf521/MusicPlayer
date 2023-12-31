2f6bf5e6aab1f021b9a8fea945f90acb849936ed
zhf521 <3407085928@qq.com>
2023-12-31 21:55:44 +0800

    渲染数据

--- src/views/MusicLibraryView.vue
+++ src/views/MusicLibraryView.vue
@@ -1,22 +1,36 @@
 <template>
   <div v-show="route.meta.showFather">
-    <el-empty v-if="true" description="空空如也~">
-      <el-button type="primary" @click="addMusic">点我去添加</el-button>
+    <el-empty v-if="musicLibrary.length === 0" description="空空如也~">
+      <el-button type="primary" @click="goToAddMusic">点我去添加</el-button>
     </el-empty>
     <div v-else class="music-list">
-      <MusicList />
+      <MusicList :list="musicLibrary" />
     </div>
   </div>
   <router-view></router-view>
 </template>
 <script setup lang="ts">
+import { useMusicLibraryStore } from '@/stores/musicLibrary';
+import { storeToRefs } from 'pinia';
+import { onMounted } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
 
 // 引入路由和路由器
 const route = useRoute();
 const router = useRouter();
-// 添加音乐到音乐库
-const addMusic = () => {
+// 引入musicLibraryStore中的变量和函数
+const musicLibraryStore = useMusicLibraryStore();
+const { musicLibrary } = storeToRefs(musicLibraryStore);
+const { loadMusicLibrary } = musicLibraryStore;
+
+// 组件挂载完执行
+onMounted(() => {
+  // 加载音乐库
+  loadMusicLibrary();
+});
+
+// 去添加音乐
+const goToAddMusic = () => {
   router.push({ name: 'add-music', params: { filename: '/' } })
 }
 </script>
