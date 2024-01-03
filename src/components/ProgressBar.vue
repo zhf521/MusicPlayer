d6ca005239c255730992b341fe9a345e4f0dee78
zhf521 <3407085928@qq.com>
2024-01-03 19:16:15 +0800

    完成点击进度条播放功能

--- src/components/ProgressBar.vue
+++ src/components/ProgressBar.vue
@@ -1,20 +1,23 @@
 <template>
   <div class="music-progress">
     <!-- 当前时间 -->
-    <div>{{ props.cTime || '00:00' }}</div>
+    <div>{{ formatTime(props.cTime) }}</div>
     <!-- 进度条 -->
     <div class="bar">
-      <div class="progress-bar" ref="progressBarRef">
+      <div class="progress-bar" ref="progressBarRef" @click="handleClick">
         <div class="played-progress" ref="playedProgressRef">
-          <span class="circle"></span>
+          <span class="circle" ref="circleRef"></span>
         </div>
       </div>
     </div>
     <!-- 总时长-->
-    <div>{{ props.dTime || '00:00' }}</div>
+    <div>{{ formatTime(props.dTime) }}</div>
   </div>
 </template>
 <script setup lang="ts">
+import { usePlayerControllerStore } from '@/stores/playerController';
+import { formatTime } from '@/utils/formatTime';
+import { storeToRefs } from 'pinia';
 import { onMounted, ref, watch } from 'vue';
 // 进度条对象
 const progressBarRef = ref(null);
@@ -23,16 +26,36 @@ const playedProgressRef = ref(null);
 // 接收的数据
 const props = defineProps(['cTime', 'dTime', 'playedProgressWidth']);
 
+// 引入playerControllerStore中的变量
+const playerControllerStore = usePlayerControllerStore();
+const { audioElement } = storeToRefs(playerControllerStore);
+
 // 组件挂载完成后执行
 onMounted(() => {
   // 设置已播放进度的初始宽度
   playedProgressRef.value.style.width = 0;
   // 监听宽度变化
   watch(() => props.playedProgressWidth, (newVal) => {
+    // 更新已播放进度的样式
     playedProgressRef.value.style.width = newVal;
   });
 })
 
+// 处理点击事件
+const handleClick = (event) => {
+  // 获取progressBarRef元素位置
+  const progressBar = progressBarRef.value.getBoundingClientRect();
+  // playedProgressRef新的宽度
+  const newWidth = `${(event.clientX - progressBar.left) / progressBar.width * 100}%`;
+  // 更新已播放进度的样式
+  playedProgressRef.value.style.width = newWidth;
+  // 总时间
+  const totalTime = props.dTime;
+  // 当前点击对应的时间
+  const currentTime = totalTime * (event.clientX - progressBar.left) / progressBar.width;
+  // 为audio设置播放当前时间
+  audioElement.value.currentTime = currentTime;
+}
 
 </script>
 <style scoped>
@@ -58,8 +81,8 @@ onMounted(() => {
 }
 
 .circle {
-  width: 1vh;
-  height: 1vh;
+  width: 2vh;
+  height: 2vh;
   border-radius: 10%;
   background-color: #000000;
 }
