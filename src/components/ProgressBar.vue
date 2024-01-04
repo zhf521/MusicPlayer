3672d80a9b6726ac84750a2fe1e710d9eaa444bf
zhf521 <3407085928@qq.com>
2024-01-04 16:03:27 +0800

    完成进度条拖动效果

--- src/components/ProgressBar.vue
+++ src/components/ProgressBar.vue
@@ -5,8 +5,7 @@
     <!-- 进度条 -->
     <div class="bar">
       <div class="progress-bar" ref="progressBarRef" @click="handleClick">
-        <div class="played-progress" ref="playedProgressRef">
-          <div class="progress-dot" ref="progressDotRef" @mousedown="handleMouseDown"></div>
+        <div class="played-progress" ref="playedProgressRef" @mousedown="handleMouseDown">
         </div>
       </div>
     </div>
@@ -36,23 +35,11 @@ const isDragging = ref(false);
 onMounted(() => {
   // 设置已播放进度的初始宽度
   playedProgressRef.value.style.width = 0;
-  // 设置已播放进度的初始宽度
-  playedProgressRef.value.style.width = 0;
-
-  // 获取进度条和小圆点的宽度
-  const progressBarWidth = progressBarRef.value.offsetWidth;
-  const dotWidth = progressDotRef.value.offsetWidth;
-
-  // 初始化小圆点的位置，使其与进度条对齐
-  progressDotRef.value.style.transform = `translateX(-${dotWidth / 2}px)`;
   // 监听宽度变化
   watch(() => props.playedProgressWidth, (newVal) => {
     if (!isDragging.value) {
       // 更新已播放进度的样式
       playedProgressRef.value.style.width = newVal;
-      // 更新小圆点的位置，根据已播放进度条的位置计算
-      const dotPosition = (parseFloat(newVal) / 100) * progressBarRef.value.offsetWidth - progressDotRef.value.offsetWidth / 2;
-      progressDotRef.value.style.transform = `translateX(${dotPosition}px)`;
     }
   });
 })
@@ -61,10 +48,6 @@ onMounted(() => {
 const handleClick = (event) => {
   // 获取progressBarRef元素位置
   const progressBar = progressBarRef.value.getBoundingClientRect();
-  // // playedProgressRef新的宽度
-  // const newWidth = `${(event.clientX - progressBar.left) / progressBar.width * 100}%`;
-  // // 更新已播放进度的样式
-  // playedProgressRef.value.style.width = newWidth;
   // 总时间
   const totalTime = props.dTime;
   // 当前点击对应的时间
@@ -73,15 +56,9 @@ const handleClick = (event) => {
   audioElement.value.currentTime = currentTime;
   // 更新已播放进度条的样式
   playedProgressRef.value.style.width = `${(event.clientX - progressBar.left) / progressBar.width * 100}%`;
-
-  // 更新小圆点的位置
-  const dotPosition = (event.clientX - progressBar.left - progressDotRef.value.offsetWidth / 2);
-  if (dotPosition >= 0 && dotPosition <= progressBar.width) {
-    progressDotRef.value.style.transform = `translateX(${dotPosition}px)`;
-  }
 }
 
-// 鼠标按下小圆点
+// 鼠标按下已播放进度条
 const handleMouseDown = (event) => {
   // 设置正在拖动的标志为true
   isDragging.value = true;
@@ -99,24 +76,17 @@ const handleMouseMove = (event) => {
   // 计算新的宽度
   let newWidth = `${(event.clientX - progressBar.left) / progressBar.width * 100}%`;
   // 检查新的宽度是否超出边界
-  if (event.clientX - progressBar.left < 0) { // 超出左边界
+  if (event.clientX < progressBar.left) { // 超出左边界
     newWidth = '0%'; // 将宽度设为0%
-  } else if (event.clientX > progressBar.right) { // 超出右边界
+  } else if (event.clientX > progressBar.left + progressBar.width) { // 超出右边界
     newWidth = '100%'; // 将宽度设为100%
   }
-  // 更新已播放进度的样式
-  playedProgressRef.value.style.width = newWidth;
-
-  // 更新小圆点的位置，根据已播放进度条的位置计算
-  const dotPosition = (event.clientX - progressBar.left - progressDotRef.value.offsetWidth / 2);
-  if (dotPosition >= 0 && dotPosition <= progressBar.width) {
-    progressDotRef.value.style.transform = `translateX(${dotPosition}px)`;
-  }
-
   // 总时间
   const totalTime = props.dTime;
   // 当前移动对应的时间
   dragCurrentTime.value = totalTime * (event.clientX - progressBar.left) / progressBar.width;
+  // 更新已播放进度的样式
+  playedProgressRef.value.style.width = newWidth;
 }
 
 // 鼠标松开事件处理函数
@@ -145,21 +115,20 @@ const handleMouseUp = (event) => {
 
 .progress-bar {
   width: 100%;
-  height: 4px;
+  height: 5px;
   background-color: #ccc;
+  cursor: pointer;
+  border-radius: 10px;
+}
+
+.progress-bar:hover {
+  height: 8px;
 }
 
 .played-progress {
   height: 100%;
   background-color: #000;
-}
-
-.progress-dot {
-  position: absolute;
-  top: -2px;
-  width: 8px;
-  height: 8px;
-  border-radius: 50%;
-  background-color: #000;
+  cursor: pointer;
+  border-radius: 10px;
 }
 </style>
