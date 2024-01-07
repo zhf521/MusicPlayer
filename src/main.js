e16d9b7d8b544c0899d346ffccf616d54b344914
zhf521 <3407085928@qq.com>
2024-01-07 20:33:06 +0800

    删除冗余代码

--- src/main.js
+++ src/main.js
@@ -4,11 +4,9 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import { createPinia } from 'pinia';
-import SvgIcon from './components/SvgIcon.vue';
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
 
 const app = createApp(App);
 const pinia = createPinia();
-app.component('SvgIcon', SvgIcon);
 app.use(router).use(pinia).use(ElementPlus).mount('#app');
