8c6cb65411978ec1a9dfb302ed474be301a3b21a
zhf521 <3407085928@qq.com>
2024-01-11 22:56:34 +0800

    完善代码逻辑

--- src/stores/userSettings.js
+++ src/stores/userSettings.js
@@ -14,17 +14,24 @@ export const useUserSettingsStore = defineStore('userSettings', () => {
             console.error('failed to load user settings', error);
         }
     };
-    const saveUserSettings = async (key, value) => {
+    const setUserSettings = async (key, value) => {
+        userSettings.value = { ...userSettings.value, [key]: value };
+    };
+    // 保存用户设置到本地
+    const saveUserSettingsToLocal = async () => {
         try {
-            const settings = { ...userSettings.value, [key]: value };
-            await localforage.setItem('userSettings', settings);
+            await localforage.setItem(
+                'userSettings',
+                JSON.parse(JSON.stringify(userSettings.value))
+            );
         } catch (error) {
-            console.error('failed to save user settings', error);
+            console.error('failed to update userSettings', error);
         }
     };
     return {
         userSettings,
         loadUserSettings,
-        saveUserSettings,
+        setUserSettings,
+        saveUserSettingsToLocal,
     };
 });
