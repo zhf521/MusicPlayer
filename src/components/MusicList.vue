7deeedeb5fc6ec55f6439cf93590c2802d3511ae
zhf521 <3407085928@qq.com>
2024-01-13 20:13:33 +0800

    基本样式完成

---
+++ src/components/MusicList.vue
@@ -0,0 +1,32 @@
+<template>
+  <el-table :data="props.list" height="100%" style="width: 100%" stripe show-overflow-tooltip>
+    <template #empty>
+      空空如也~
+    </template>
+    <el-table-column type="index" />
+    <el-table-column label="标题">
+      <template #default="scope">
+        <div @dblclick="selectPlay(scope.$index)" style="overflow: hidden; text-overflow: ellipsis;">
+          {{ scope.row.tag && scope.row.tag.tags.title || scope.row.basename }}
+        </div>
+      </template>
+    </el-table-column>
+    <el-table-column label="艺术家">
+      <template #default="scope">
+        {{ scope.row.tag && scope.row.tag.tags.artist || '未知艺术家' }}
+      </template>
+    </el-table-column>
+    <el-table-column prop="album" label="专辑">
+      <template #default="scope">
+        {{ scope.row.tag && scope.row.tag.tags.album || '未知专辑' }}
+      </template>
+    </el-table-column>
+  </el-table>
+</template>
+<script setup>
+const props = defineProps({ list: Array });
+const selectPlay = (index) => {
+  console.log(index);
+};
+</script >
+<style scoped></style>;
