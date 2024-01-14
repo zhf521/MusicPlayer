e1538430046a9d19bdc54b40705215a3bdeaaf29
zhf521 <3407085928@qq.com>
2024-01-14 21:26:30 +0800

    删除冗余代码

--- src/views/CloudFilesView.vue
+++ src/views/CloudFilesView.vue
@@ -97,7 +97,7 @@ watch(() => route.params.filename, async (newVal) => {
   loading.value = true;
   try {
     await getDirectory(newVal);
-    console.log(contents.value);
+    // console.log(contents.value);
   } catch (error) {
     console.log(error);
   } finally {
