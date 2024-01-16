46a504442e2923e349f42e7970fe5dd2cb6a2e83
zhf521 <3407085928@qq.com>
2024-01-16 22:54:53 +0800

    更新主页

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,39 @@
 <template>
-  <el-container>
-    <el-header class="flex-vertical-center">
+  <div class="container">
+    <div class="header">
       <Header />
-    </el-header>
-    <el-container style="height: calc(100vh - 130px);">
-      <Aside />
-      <el-main>
-        <router-view />
-      </el-main>
-    </el-container>
-    <Footer />
-  </el-container>
+    </div>
+    <div class="main">
+      <router-view></router-view>
+    </div>
+    <div class="footer">
+      <Footer />
+    </div>
+  </div>
 </template>
 <script setup>
 import Header from '../components/Header.vue';
-import Aside from '../components/Aside.vue';
 import Footer from '../components/Footer/Footer.vue';
 </script>
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.container {
+  width: 100vw;
+  height: 100vh;
+
+  .header {
+    width: 100%;
+    height: 60px;
+  }
+
+  .main {
+    width: 100%;
+    height: calc(100% - 60px - 105px);
+  }
+
+  .footer {
+    width: 100%;
+    height: 105px;
+    border-top: 1px solid var(--el-border-color);
+  }
+}
+</style>
