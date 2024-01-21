9210de7016d1be079dca2f475c1e4d5ab7ccfdb9
zhf521 <3407085928@qq.com>
2024-01-21 09:56:44 +0800

    删除历史代码

--- src/views/SettingsView-h.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <h2>设置</h2>
-  <h3>WebDav设置</h3>
-  <WebDavSettings />
-</template>
-<script setup>
-import WebDavSettings from '@/components/WebDavSettings.vue';
-</script>
-<style scoped></style>
