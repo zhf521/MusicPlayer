c8b2485b3bc4c6bf782cfa60e8e61db8b08a864d
zhf521 <3407085928@qq.com>
2024-01-24 21:43:32 +0800

    完善音乐库

--- src/views/MusicLibraryView.vue
+++ src/views/MusicLibraryView.vue
@@ -1,28 +1,64 @@
 <template>
   <div class="music-library-container">
-    <div class="empty">
+    <div class="empty" v-if="musicLibrary.length === 0">
       空空如也~
+      <button @click="goToCloudFiles">
+        点击添加音乐
+      </button>
     </div>
-    <div class="music-list">
-
+    <div class="music-library">
+      音乐库
     </div>
   </div>
 </template>
 <script setup>
+import { useRouter } from 'vue-router';
+import { useMusicLibraryStore } from '../stores/musicLibrary.js';
+import { storeToRefs } from 'pinia';
+
+// 引入musicLibraryStore中的变量
+const musicLibraryStore = useMusicLibraryStore();
+const { musicLibrary } = storeToRefs(musicLibraryStore);
+
+// 引入路由器
+const router = useRouter();
+// 去云文件
+const goToCloudFiles = () => {
+  router.push({ name: 'cloud-files', params: { filename: '/' } });
+};
 </script>
 <style scoped lang="less">
 .music-library-container {
   width: 100%;
   height: 100%;
   padding: 0 20px;
-  background-color: pink;
 
   .empty {
     width: 100%;
     height: 100%;
-    background-color: green;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    button {
+      margin-top: 20px;
+      font-size: 17px;
+      padding: 10px 20px;
+      border: transparent;
+      box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.4);
+      background-color: skyblue;
+      border-radius: 4px;
+
+      &:active {
+        scale: 0.95;
+      }
+    }
   }
 
-  .music-list {}
+  .music-library {
+    width: 100%;
+    height: 100%;
+  }
 }
 </style>
